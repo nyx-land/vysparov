fa9fd505995c6d70fa0528205387679ef3ba3c7f
Nyx <n1x@riseup.net>
2023-06-11 01:44:01 -0700

    fix package exports

--- src/package.lisp
+++ src/package.lisp
@@ -1,20 +1,13 @@
 (defpackage #:vysparov
   (:nicknames :vy)
   (:use :cl)
-  '(:export :split
-    :hash-print
-    :dict
-    :with-keys
-    :deepget
-    :deepset
-    :xyphenate
-    :sethash
-    :str->octets
-    :octets->str
-    :cconv
-    :make-tr-node
-    :tr-add
-    :tr-lookup
-    :peek-nth-chars))
+  (:export
+   :split :hash-print
+   :dict :with-keys
+   :deepget :deepset
+   :xyphenate :str->octets
+   :octets->str
+   :make-tr-node :tr-add
+   :tr-lookup :peek-nth-chars))
 
 (in-package :vysparov)
